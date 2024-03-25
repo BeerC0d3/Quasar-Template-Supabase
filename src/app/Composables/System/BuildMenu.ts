6293d697e4e02db85c3b55868793d710d4d6e6a4
Diego luna <beerc0d3@gmail.com>
2024-03-25 13:00:24 -0600

    useApi update

--- src/app/Composables/System/BuildMenu.ts
+++ src/app/Composables/System/BuildMenu.ts
@@ -2,13 +2,13 @@ import { IMenu, IMenuEntity } from 'src/app/Models/System/IModel';
 import useApi from 'src/app/Composables/UseApi';
 import { ref } from 'vue';
 
-const { CollectionTable } = useApi();
+const { TableToList } = useApi();
 const menus = ref<IMenu[]>([]);
 const menuEntity = ref<IMenuEntity[]>([]);
 
 const getMenu = async () => {
   try {
-    menuEntity.value = await CollectionTable('system', 'menu');
+    menuEntity.value = await TableToList('system', 'menu');
     CreateMenu(menuEntity.value);
   } catch (error: any) {
     console.log(error);
@@ -18,7 +18,7 @@ const getMenu = async () => {
 const CreateMenu = (menuEntity: IMenuEntity[]) => {
   const menuOrigin = menuEntity.find((item) => item.parentid == 0);
   if (menuOrigin == null || menuOrigin == undefined)
-    throw 'No se encontro el menu origen';
+    throw 'The source menu was not found';
 
   menus.value.push({
     id: menuOrigin.id,
--- src/app/Composables/UseApi.ts
+++ src/app/Composables/UseApi.ts
@@ -3,12 +3,12 @@ import useSupabase from 'src/boot/supabase';
 export default function useApi() {
   const { supabase } = useSupabase();
 
-  const CollectionTable = async (schema: string, table: string) => {
+  const TableToList = async (schema: string, table: string) => {
     const { data, error } = await supabase.schema(schema).from(table).select();
     if (error) throw error;
 
     return data;
   };
 
-  return { CollectionTable };
+  return { TableToList };
 }
